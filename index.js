8eed521758549aa378d9d356a75ce44b3dd6cacb
Miles <wangle2071@.com>
2025-07-18 11:52:32 +0800

    v202507181152

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const supabase = require("./supabase");
+
 const cors = require("cors");
 const router = require("./router/router");
 
@@ -8,9 +10,28 @@ const router = require("./router/router");
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// 查询数据
+// router
 app.use(router);
 
-app.listen(port, '192.168.120.233', () => {
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password
+  });
+  // console.log(data);
+  // res.end('OK');
+  if (error) {
+    res.status(500).send(error);
+  } else {
+    res.json({    
+      code: 200,
+      message: '登录成功',
+      data: data
+    })
+  }
+})
+
+app.listen(port, '192.168.8.5', () => {
   console.log(`Server is running on port ${port}`)
 })
