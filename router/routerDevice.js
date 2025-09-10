e6b87e99eeac09547a55d9e0ad859aef13d9ee52
Miles <wangle2071@163.com>
2025-09-10 16:36:18 +0800

    完成新增设备接口

--- router/routerDevice.js
+++ router/routerDevice.js
@@ -37,11 +37,87 @@ router.post('/DeleteDevice', verifyToken, async (req, res) => {
       message: '删除失败',
       data: error
     })
-  }
-  else {
+  }else {
     res.json({
       code: 200,
       message: '删除成功',
+      data: data
+    })
+  }
+})
+
+//获取设备类型
+router.get('/DeviceType', verifyToken, async (req, res) => {
+  const {data, error} = await supabase.from('product_type_cn')
+  .select('*')
+
+  if (error) {
+    res.json({
+      code: 400,
+      message: '获取数据失败',
+      data: error
+    })
+  }else {
+    res.json({
+      code: 200,
+      message: '获取数据成功',
+      data: data
+    })
+  }
+})
+
+// 根据设备类型关联查询设备品牌
+router.get('/DeviceBrand', verifyToken, async (req, res) => {
+  const { deviceTypeText } = req.query
+  
+  const {data, error} = await supabase.from('product_type_cn')
+  .select(`key, product_brand_cn(value, brand_id, logo_url)`)
+  .eq('key', deviceTypeText)
+
+  if (error) {
+    res.json({
+      code: 400,
+      message: '获取数据失败',
+      data: error
+    })
+  }else {
+    res.json({
+      code: 200,
+      message: '获取数据成功',
+      data: data
+    })
+  }
+})
+
+// 新增设备
+router.post('/AddDevice', verifyToken, async (req, res) => {
+  const { device_name, device_type, device_brand, device_price, device_number, device_remark, device_time, device_update, device_user, device_logo } = req.body
+  
+  const {data, error} = await supabase.from('it_assets_cn')
+  .insert({
+    product_name: device_name,
+    product_type: device_type,
+    product_brand: device_brand,
+    product_price: device_price,
+    product_number: device_number,
+    product_remark: device_remark,
+    product_time: device_time,
+    product_user: device_user,
+    product_logo: device_logo,
+    product_update: device_update,
+    value: device_name
+  })
+
+  if (error) {
+    res.json({
+      code: 400,
+      message: '新增失败',
+      data: error
+    })
+  }else {
+    res.json({
+      code: 200,
+      message: '新增成功',
       data: data
     })
   }
